1821822d899516a35604fa14e7714838a7b4191c
Sezgin Zerdelci <sezginzerdelci@gmail.com>
2019-08-26 10:24:25 +0300

    Car Request modüle dosyaları eklendi.

---
+++ controllers/controllers.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+class EmployeeCarRequest(http.Controller):
+
+    @http.route('/api/create_car_request', auth='public', csrf=False, type='json', methods=['POST'])
+    def create_car_request(self, **kw):
+        print(http.request)
+        print(kw)
+        new_car_request = http.request.env['car.request'].sudo().create(kw)
+
+        if new_car_request:
+            return "The car request has been created"
+        else:
+            return "No car request"
+
+    # @http.route('/employee_car_request/employee_car_request/', auth='public')
+    # def index(self, **kw):
+    #     return "Hello, Odoo Live Session"
+
+#     @http.route('/employee_car_request/employee_car_request/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('employee_car_request.listing', {
+#             'root': '/employee_car_request/employee_car_request',
+#             'objects': http.request.env['employee_car_request.employee_car_request'].search([]),
+#         })
+
+#     @http.route('/employee_car_request/employee_car_request/objects/<model("employee_car_request.employee_car_request"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('employee_car_request.object', {
+#             'object': obj
+#         })
---
+++ models/models.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class CarRequest(models.Model):
+    _name = "car.request" # Table in DB => car_request
+    _inherit = ['mail.thread']
+    _description = "Car Request"
+
+    name = fields.Char(string="Request", required=True, )
+    date_from = fields.Datetime(string="Starting Date", default=fields.Datetime.now(), )
+    date_to = fields.Datetime(string="End Date", required=False, )
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
+    car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
+    state = fields.Selection(string="Status", selection=[('draft', 'Draft'), ('confirm', 'Confirm'),
+                                                         ('validate', 'Validated'), ('refuse', 'Refuse'),
+                                                         ('approved', 'Approved'), ], default="draft", track_visibility='onchange', )
+    email = fields.Char(string="Email", required=False, )
+    website = fields.Char(string="Website", required=False, )
+
+    @api.onchange('email')
+    def _onchange_email(self):
+        result = {}
+        if self.email:
+            # self.website = 'http://www.%s' % (self.email.split('@')[1])
+            result.update({
+                'value': {
+                    'website': 'http://www.%s' % (self.email.split('@')[1])
+                },
+                'warning': {
+                    'title': 'Congrates!',
+                    'message': 'You have added an email!',
+                },
+                'domain': {
+                    'employee_id': [('id', '!=', 20)],
+                }
+            })
+
+        return result
+
+    _sql_constraints = [
+        ('unique_email', 'unique(email)', 'The email should be unique!'),
+    ]
+
+    @api.constrains('email')
+    def _check_email(self):
+        if self.email.endswith('gmail.com'):
+            raise ValidationError("Gmail is not accepted!")
+        if self.email.endswith('yahoo.com'):
+            raise ValidationError("Yahoo is not accepted!")
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+
+    @api.multi
+    def approve_request(self):
+        self.state = 'approved'
